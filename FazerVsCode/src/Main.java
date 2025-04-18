834f87ff53724299aa0e831ee7565e4558d39918
Tainá Kasai <136513139+tahkasai@users.noreply.github.com>
2025-04-17 21:01:20 -0300

    update main and class operadores

--- FazerVsCode/src/Main.java
+++ FazerVsCode/src/Main.java
@@ -5,11 +5,29 @@ public class Main {
     public static void funcao_teste (){
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Digite o primeiro número: ");
         float v1 = scanner.nextFloat();
+        System.out.println("Digite o segundo numero: ");
         float v2 = scanner.nextFloat();
 
         Operadores ops = new Operadores(v1,v2);
 
-        System.out.println(ops.getP1()+" "+ops.getP2());
+        System.out.println("Digite a operação (1- adição, 2- subtração, 3- multiplicação e 4- divisão ");
+        int operacao = scanner.nextInt();
+
+        switch (operacao){
+            case 1 -> System.out.println("O resultado é: "+ops.adicao(v1,v2));
+            case 2 -> System.out.println("O resultado é: "+ops.subtracao(v1,v2));
+            case 3 -> System.out.println("O resultado é: "+ops.multiplicacao(v1,v2));
+            case 4 -> {
+                if (v2 == 0){
+                    System.out.println("Não existe divisão com zero");
+                }else{
+                    System.out.println("O resultado é: "+ops.divisao(v1,v2));
+                }
+            }
+            default -> System.out.println("Não existe essa operação");
+        }
+
     }
 }
--- FazerVsCode/src/Models/Operadores.java
+++ FazerVsCode/src/Models/Operadores.java
@@ -17,4 +17,19 @@ public class Operadores {
     public float getP2() {
         return p2;
     }
+
+    // operações
+    public float adicao(float v1, float v2){
+        return v1+v2;
+    }
+    public float subtracao(float v1, float v2){
+        return v1-v2;
+    }
+    public float multiplicacao(float v1, float v2){
+        return v1*v2;
+    }
+    public float divisao(float v1, float v2){
+        return v1/v2;
+    }
+
 }
