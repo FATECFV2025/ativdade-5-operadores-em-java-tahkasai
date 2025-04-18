026028ea38848094de643bfcedf5b2c67a5f1aca
Tainá Kasai <136513139+tahkasai@users.noreply.github.com>
2025-04-17 21:14:28 -0300

    Update Main.java

--- FazerVsCode/src/Main.java
+++ FazerVsCode/src/Main.java
@@ -12,18 +12,34 @@ public class Main {
 
         Operadores ops = new Operadores(v1,v2);
 
-        System.out.println("Digite a operação:\n(1- adição, 2- subtração, 3- multiplicação e 4- divisão) ");
+        System.out.print("(1- adição, 2- subtração, 3- multiplicação e 4- divisão)\nDigite a operação: ");
         int operacao = scanner.nextInt();
 
+//        if (operacao==1){
+//            System.out.println("O resultado é: "+ops.adicao(v1,v2));
+//        } else if (operacao==2) {
+//            System.out.println("O resultado é: "+ops.subtracao(v1,v2));
+//        } else if (operacao==3) {
+//            System.out.println("O resultado multiplicação é: "+ops.multiplicacao(v1,v2));
+//        } else if(operacao==4){
+//            if (v2 == 0){
+//                System.out.println("Não existe divisão com zero");
+//            } else{
+//                System.out.println("O resultado da divisão é: "+ops.divisao(v1,v2));
+//            }
+//        } else{
+//            System.out.println("Não existe essa operação");
+//        }
+
         switch (operacao){
-            case 1 -> System.out.println("O resultado é: "+ops.adicao(v1,v2));
-            case 2 -> System.out.println("O resultado é: "+ops.subtracao(v1,v2));
-            case 3 -> System.out.println("O resultado é: "+ops.multiplicacao(v1,v2));
+            case 1 -> System.out.println("O resultado da adição é: "+ops.adicao(v1,v2));
+            case 2 -> System.out.println("O resultado da subtração é: "+ops.subtracao(v1,v2));
+            case 3 -> System.out.println("O resultado multiplicação é: "+ops.multiplicacao(v1,v2));
             case 4 -> {
                 if (v2 == 0){
                     System.out.println("Não existe divisão com zero");
                 }else{
-                    System.out.println("O resultado é: "+ops.divisao(v1,v2));
+                    System.out.println("O resultado da divisão é: "+ops.divisao(v1,v2));
                 }
             }
             default -> System.out.println("Não existe essa operação");
