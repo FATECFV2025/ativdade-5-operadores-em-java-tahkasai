a8b5fcd96e8f4d269c285c1735054669995eca60
Tainá Kasai <136513139+tahkasai@users.noreply.github.com>
2025-04-23 18:32:37 -0300

    update main and operadores

--- FazerVsCode/src/Main.java
+++ FazerVsCode/src/Main.java
@@ -15,35 +15,27 @@ public class Main {
         System.out.print("(1- adição, 2- subtração, 3- multiplicação e 4- divisão)\nDigite a operação: ");
         int operacao = scanner.nextInt();
 
-//        if (operacao==1){
-//            System.out.println("O resultado é: "+ops.adicao(v1,v2));
-//        } else if (operacao==2) {
-//            System.out.println("O resultado é: "+ops.subtracao(v1,v2));
-//        } else if (operacao==3) {
-//            System.out.println("O resultado multiplicação é: "+ops.multiplicacao(v1,v2));
-//        } else if(operacao==4){
-//            if (v2 == 0){
-//                System.out.println("Não existe divisão com zero");
-//            } else{
-//                System.out.println("O resultado da divisão é: "+ops.divisao(v1,v2));
-//            }
-//        } else{
-//            System.out.println("Não existe essa operação");
-//        }
 
-        switch (operacao){
-            case 1 -> System.out.println("O resultado da adição é: "+ops.adicao(v1,v2));
-            case 2 -> System.out.println("O resultado da subtração é: "+ops.subtracao(v1,v2));
-            case 3 -> System.out.println("O resultado multiplicação é: "+ops.multiplicacao(v1,v2));
-            case 4 -> {
-                if (v2 == 0){
-                    System.out.println("Não existe divisão com zero");
-                }else{
-                    System.out.println("O resultado da divisão é: "+ops.divisao(v1,v2));
-                }
+        if (operacao==1){
+            System.out.println("O resultado é: "+ops.adicao(v1,v2));
+        } else if (operacao==2) {
+            System.out.println("O resultado é: "+ops.subtracao(v1,v2));
+        } else if (operacao==3) {
+            System.out.println("O resultado multiplicação é: "+ops.multiplicacao(v1,v2));
+        } else if(operacao==4){
+            if (v2 == 0){
+                System.out.println("Não existe divisão com zero");
+            } else{
+                System.out.println("O resultado da divisão é: "+ops.divisao(v1,v2));
             }
-            default -> System.out.println("Não existe essa operação");
+        } else{
+            System.out.println("Não existe essa operação");
         }
 
+        System.out.println();
+        System.out.println("Desafio da atividade");
+        System.out.println("Resultado com operadores de atribuição (p1 += p2): "+ops.atribuicao());
+        System.out.println("Resultado com operadores de atribuição: "+ ops.comparacao());
+        System.out.println("Resultado com operadores lógicos: "+ops.logigo());
     }
 }
--- FazerVsCode/src/Models/Operadores.java
+++ FazerVsCode/src/Models/Operadores.java
@@ -18,7 +18,7 @@ public class Operadores {
         return p2;
     }
 
-    // operações
+    // operações básicas
     public float adicao(float v1, float v2){
         return v1+v2;
     }
@@ -32,4 +32,31 @@ public class Operadores {
         return v1/v2;
     }
 
+    // Desafio da Atividade
+    // Operadores de atribuição
+    public float atribuicao(){
+        float valor = p1;
+        valor += p2;
+        return valor;
+    }
+    // Operadores de comparação
+    public String comparacao(){
+        if(p1 == p2){
+            return "Os valores são iguais;";
+        } else if(p1 > p2){
+            return "O primeiro valor é maior que o segundo.";
+        } else{
+            return "O segundo valor é maior que o primeiro";
+        }
+    }
+    // Operadores lógico
+    public String logigo(){
+        if (p1 > 0 && p2 > 0){
+            return "Ambos os valores são positivos.";
+        } else  if (p1 > 0 || p2 > 0){
+            return "Pelo menos um valor é positivo.";
+        } else{
+            return "Nenhum valor positivo.";
+        }
+    }
 }
